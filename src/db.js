94362b1d74b5cf85d3b8669093499876db806dc0
Luciane <costaluciane087@gmail.com>
2021-09-15 00:09:05 -0300

    finalizando

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import Sequelize from 'sequelize';
 
 const sequelize = new Sequelize(
     'mysql_17753_nsf2021',
-    ' nsf2021',
+    'nsf2021',
     'nsf@2021', { 
     host: 'my01.winhost.com',
     dialect: 'mysql',
